df280b3e7bce9f2629b06cf13d146bba35333faa
Cherif Redissi <cherifredissi@hotmail.com>
2020-09-20 12:42:16 -0400

    test: refactor tests

---
+++ Memcached.Mimic.Tests/CommandParserTests.cs
@@ -0,0 +1,53 @@
+using Memcached.Mimic.Commands;
+using Memcached.Mimic.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Memcached.Mimic.Tests
+{
+    [TestClass]
+    public class CommandParserTests
+    {
+        [TestMethod]
+        public void GetCommandParses_UserInput()
+        {
+            GetCommand command = CommandParser.ParseFromUserInput("      get a", false) as GetCommand;
+            Assert.AreEqual("a", command.Key);
+
+            command = CommandParser.ParseFromUserInput("      get otherKey", false) as GetCommand;
+            Assert.AreEqual("otherKey", command.Key);
+
+            command = CommandParser.ParseFromUserInput("      gget other___Key", false) as GetCommand;
+            Assert.AreEqual(command, null);
+        }
+        [TestMethod]
+        public void GetCommandParses_Sent()
+        {
+            GetCommand command = CommandParser.ParseFromSentData("      get a") as GetCommand;
+            Assert.AreEqual("a", command.Key);
+            command = CommandParser.ParseFromSentData("      get otherKey") as GetCommand;
+            Assert.AreEqual("otherKey", command.Key);
+            command = CommandParser.ParseFromSentData("      gget other Key") as GetCommand;
+            Assert.AreEqual(command, null);
+
+
+        }
+        [TestMethod]
+        public void SetCommandParses_UserInput()
+        {
+            SetCommand command = CommandParser.ParseFromUserInput("      set myKey 1024", false, "someKeyValue") as SetCommand;
+            Assert.AreEqual("myKey", command.Key);
+            Assert.AreEqual(1024, command.ValueSize);
+            Assert.AreEqual("someKeyValue", command.Value);
+        }
+        [TestMethod]
+        public void SetCommandParses_Sent()
+        {
+            SetCommand command = CommandParser.ParseFromSentData("      set myKey 1024 someKeyValue") as SetCommand;
+            Assert.AreEqual("myKey", command.Key);
+            Assert.AreEqual(1024, command.ValueSize);
+            Assert.AreEqual("someKeyValue", command.Value);
+
+
+        }
+    }
+}
