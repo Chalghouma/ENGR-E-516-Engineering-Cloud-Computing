c9b8b725bff16e3e4740d7576fb3597bd4cea69b
Cherif Redissi <cherifredissi@hotmail.com>
2020-09-20 10:47:11 -0400

    chore: add Client & Server separate apps

---
+++ Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Memcached.Mimic.Client.Client("127.0.0.1",10001);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Memcached.Mimic.Server.Server("127.0.0.1",10001).ListenForClients();
+        }
+    }
+}
