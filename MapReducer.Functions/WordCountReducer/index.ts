1b3753d3937d4dd2548286d9073872aaeb7e2d37
Cherif Redissi <cherifredissi@hotmail.com>
2020-10-10 16:46:12 -0400

    feat: WordCountReducer in AzureFunction

---
+++ MapReducer.Functions/WordCountReducer/index.ts
@@ -0,0 +1,22 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { reduce, map } from "lodash";
+const fs = require("fs");
+interface KeyValuePairInput {
+  Key: string;
+  Value: number;
+}
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const items = req.body as KeyValuePairInput[];
+  const occurences = map(items, (item) => item.Value);
+  const sum = reduce(occurences, (accumulated, current) => accumulated + current);
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: { Key: items[0].Key, Value: sum },
+  };
+};
+
+export default httpTrigger;
