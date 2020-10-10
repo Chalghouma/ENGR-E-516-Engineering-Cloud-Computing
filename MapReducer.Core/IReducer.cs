cbb302960bfb517d08331da99204f9c3b5c45494
Cherif Redissi <cherifredissi@hotmail.com>
2020-10-10 10:51:00 -0400

    chore: define MapReducer.core

---
+++ MapReducer.Core/IReducer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapReducer.Core
+{
+    interface IReducer<TInput,KOutput>
+    {
+        KOutput Reduce(TInput input);
+    }
+}
