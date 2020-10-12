47ef4afa38c64540c8176d06a4aa767b8a891d2c
Cherif Redissi <cherifredissi@hotmail.com>
2020-10-12 11:42:20 -0400

    feat: add ConsoleFileLogger

---
+++ MapReducer.Core/Logger/ConsoleFileLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MapReducer.Core.Logger
+{
+    public class ConsoleFileLogger : ILogger
+    {
+        string _filePath;
+        object locker = new object();
+        public ConsoleFileLogger(string filePath)
+        {
+            _filePath = filePath;
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+        }
+
+        public void Log(string message)
+        {
+            string formatted = $"[{DateTime.Now}] - {message}";
+            Console.WriteLine(formatted);
+            lock (locker)
+            {
+                File.AppendAllLines(_filePath, new string[] { formatted });
+            }
+        }
+    }
+}
